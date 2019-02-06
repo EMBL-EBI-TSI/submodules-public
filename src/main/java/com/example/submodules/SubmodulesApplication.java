896549391f7f3e344a4e9175c2dbd41648081070
navisatebi <navis@ebi.ac.uk>
2019-02-06 11:38:50 +0000

    introduce yourself in Dev

--- src/main/java/com/example/submodules/SubmodulesApplication.java
+++ src/main/java/com/example/submodules/SubmodulesApplication.java
@@ -1,12 +1,24 @@
 package com.example.submodules;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SubmodulesApplication {
+public class SubmodulesApplication implements CommandLineRunner {
 
+	@Autowired
+	private SubService subService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SubmodulesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Change which will go from Dev to Master");
+		System.out.println(subService.introduceYourself());
+		
 	}
 }
